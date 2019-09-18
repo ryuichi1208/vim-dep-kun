768418de2cdc02eacf773cef8accb2aa9a74468e
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-18 23:14:02 +0900

    first commit

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,13 +3,19 @@
 GIT_CMD=$(which git)
 GIT_SCHE="https://"
 GIT_USER="ryuichi1208"
+GIT_MAIL="ryucrosskey@gmail.com"
 GIT_PASS="Abjlfhy31"
 GIT_REPO="vim-dep-kun"
 
 ${GIT_CMD} clone ${GIT_SCHE}${GIT_USER}:${GIT_PASS}@github.com/${GIT_USER}/${GIT_REPO}.git
 ls -Rl
 
+uname -a
+
 cd ${GIT_REPO}
+
+${GIT_CMD} config user.name ${GIT_USER}
+${GIT_CMD} config user.email ${GIT_MAIL}
 
 GIT_OLD_TAG=$(cat tag)
 GIT_NEW_TAG=$(echo "${GIT_OLD_TAG} + 0.1" | bc)
