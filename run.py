4f31d11de54052b8812861ff96ee15185191c0e7
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-18 22:29:15 +0900

    first commit

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
 import os
-from flask import Flask
+
+from subprocess import check_output
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +13,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+@app.route("/api/commands")
+def exec_commands() -> str:
+    cmd_info = {
+        "git" : f"{check_output(['git', 'version'])}",
+        "python" : f"{check_output(['python3', '-V'])}"
+    }
+    return jsonify(cmd_info)
 
 
 if __name__ == '__main__':
