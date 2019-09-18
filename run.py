d1d2adcc6e7b7647dbdabcfd5f260c1744261c0c
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-18 22:15:33 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
