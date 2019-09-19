fe13b2d515042d6686c0cf95acf50417f904b752
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-19 17:00:51 +0900

    first commit

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,19 +7,48 @@ GIT_MAIL="ryucrosskey@gmail.com"
 GIT_PASS="Abjlfhy31"
 GIT_REPO="vim-dep-kun"
 
-${GIT_CMD} clone ${GIT_SCHE}${GIT_USER}:${GIT_PASS}@github.com/${GIT_USER}/${GIT_REPO}.git
-ls -Rl
+function git_clone()
+{
+  ${GIT_CMD} clone ${GIT_SCHE}${GIT_USER}:${GIT_PASS}@github.com/${GIT_USER}/${GIT_REPO}.git
+  ls -Rl
+  cd ${GIT_REPO}
 
-cd ${GIT_REPO}
+  ${GIT_CMD} config user.name ${GIT_USER}
+  ${GIT_CMD} config user.email ${GIT_MAIL}
+}
 
-${GIT_CMD} config user.name ${GIT_USER}
-${GIT_CMD} config user.email ${GIT_MAIL}
+function git_push() {
+  local _GIT_NEW_TAG=$1
 
-GIT_OLD_TAG=$(cat tag)
-GIT_NEW_TAG=$(echo "${GIT_OLD_TAG}" | awk '{print $1+0.1}')
+  ${GIT_CMD} add .
+  ${GIT_CMD} commit -m "tag new ${_GIT_NEW_TAG}"
+  ${GIT_CMD} push
+}
 
-sed -i -e "s/${GIT_OLD_TAG}/${GIT_NEW_TAG}/g" tag
+function git_tag_push()
+{
+  ${GIT_CMD} tag -a ${GIT_NEW_TAG} -m "New tag ${GIT_NEW_TAG}"
+  ${GIT_CMD} show
+  ${GIT_CMD} push origin --tags
 
-${GIT_CMD} add .
-${GIT_CMD} commit -m "tag new ${GIT_NEW_TAG}"
-${GIT_CMD} push
+}
+
+function main()
+{
+
+  # Git関連の初期化
+  git_clone
+
+  GIT_OLD_TAG=$(cat tag)
+  GIT_NEW_TAG=$(echo "${GIT_OLD_TAG}" | awk '{print $1+0.1}')
+
+  sed -i -e "s/${GIT_OLD_TAG}/${GIT_NEW_TAG}/g" tag
+
+  # GitHubへの通常push
+  git_push ${GIT_NEW_TAG}
+
+  # GitHubへのTag漬けを実行
+  #git_tag_push
+}
+
+main
