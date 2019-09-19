b35c8dddc1a8012de8e01dfa8467b3b0557b356e
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-19 17:17:14 +0900

    first commit

--- run.py
+++ run.py
@@ -6,21 +6,38 @@ import os
 
 from subprocess import check_output
 from flask import Flask, jsonify
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def do_basic_auth(id):
+    id_list = {
+        "admin": "admin"
+    }
+
+    if id in id_list:
+        return id_list.get(id)
+    return None
 
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+
 @app.route("/api/git")
+@auth.login_required
 def git_push():
     out = check_output(["bash", "./script/deploy.sh"])
-    return out
+    result = {'a': 'b'}
+    return jsonify(result), 201
 
 
 @app.route("/api/commands")
+@auth.login_required
 def exec_commands() -> str:
     cmd_info = {
         "git" : f"{check_output(['git', 'version'])}".rstrip('\n'),
